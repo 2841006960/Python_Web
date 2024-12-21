5ebbf3708fd3e0edfaa672f09168f78f9866828c
YosparNa <2730094242@qq.com>
2024-12-21 19:51:26 +0800

    19.3.1

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -21,7 +21,7 @@ def topics(request):
 
 
 
-
+@login_required
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
     topic = Topic.objects.get(id=topic_id)
@@ -31,7 +31,7 @@ def topic(request, topic_id):
 
 
 
-
+@login_required
 def new_topic(request):
     """添加新主题"""
     if request.method != 'POST':
@@ -48,7 +48,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
-
+@login_required
 def new_entry(request, topic_id):
     """在特定主题中添加新条目"""
     topic = Topic.objects.get(id=topic_id)
@@ -68,7 +68,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
-
+@login_required
 def edit_entry(request, entry_id):
     """编辑既有的条目"""
     entry = Entry.objects.get(id=entry_id)
