612e02c85612da06d86d0b43380c4cb80770660d
YosparNa <2730094242@qq.com>
2024-12-21 20:02:52 +0800

    19.3.4　保护用户的主题

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -4,6 +4,7 @@ from .forms import TopicForm
 from .forms import TopicForm, EntryForm
 from .models import Topic, Entry
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -25,6 +26,10 @@ def topics(request):
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
     topic = Topic.objects.get(id=topic_id)
+    # 确认请求的主题属于当前用户
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
