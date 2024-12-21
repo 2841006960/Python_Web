e99ac513c7578bb60030b7aa0bcecd5db5dc51db
YosparNa <2730094242@qq.com>
2024-12-21 18:28:03 +0800

    19.1.2　添加新条目

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
 from .models import Topic
 from .forms import TopicForm
+from .forms import TopicForm, EntryForm
+
+
 # Create your views here.
 def index(request):
     """学习笔记的主页"""
     return render(request, 'learning_logs/index.html')
+
+
 
 
 def topics(request):
@@ -14,12 +19,16 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+
+
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
 
 
 def new_topic(request):
@@ -37,3 +46,22 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+
+
+def new_entry(request, topic_id):
+    """在特定主题中添加新条目"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # 未提交数据：创建一个空表单
+        form = EntryForm()
+    else:
+        # POST 提交的数据：对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    # 显示空表单或指出表单数据无效
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
